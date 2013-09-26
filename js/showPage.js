d3bd4270ca06968c1a972533e8188e039b8b8ae7
xuefeng wu <wuxuef2@qq.com>
2013-09-26 22:12:11 +0800

    fix button

--- js/showPage.js
+++ js/showPage.js
@@ -1,5 +1,11 @@
-﻿window.onload = function () {
-    var screenWidth = document.body.offsetWidth;
+﻿var pageEventSeter = function () {
+    setHeadImgWidth();
+};
+
+var setHeadImgWidth = function () {
+    var screenWidth = document.body.clientWidth;
     document.getElementById("headImg").setAttribute("width", screenWidth + "px");
     document.getElementById("headImg").setAttribute("min-width", "960px");
 };
+
+myWindowOnloadFunction(pageEventSeter);
