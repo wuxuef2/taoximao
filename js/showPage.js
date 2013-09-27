aa09f2efa1de32099d7750fbcbcb81823c70a1d9
xuefeng wu <wuxuef2@qq.com>
2013-09-27 19:20:06 +0800

    get change

--- js/showPage.js
+++ js/showPage.js
@@ -4,8 +4,14 @@
 
 var setHeadImgWidth = function () {
     var screenWidth = document.body.clientWidth;    
-    $("#headImg").attr("width", screenWidth + "px");
-    document.getElementById("headImg").setAttribute("min-width", "960px");
+    
+    if (navigator.userAgent.indexOf("MSIE 7.0") > 0) {
+        document.getElementById("headImg").style.width = screenWidth + "px";
+    }
+    else {
+        $("#headImg").attr("width", screenWidth + "px");
+        document.getElementById("headImg").setAttribute("min-width", "960px");
+    }
 };
 
 myWindowOnloadFunction(pageEventSeter);
